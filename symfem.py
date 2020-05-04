34dd71b27cc0c6c105c51d2711ab348de5c44d52
Dimas Abreu Archanjo Dutra <dimasad@ufmg.br>
2020-05-04 15:59:52 -0300

    Adding symbolic natural formulation model.

--- symfem.py
+++ symfem.py
@@ -6,6 +6,7 @@ import numpy as np
 import sympy
 from ceacoest.modelling import symoptim
 
+
 class InnovationDTModel(symoptim.Model):
     def __init__(self, nx, nu, ny):
         super().__init__()
@@ -19,11 +20,8 @@ class InnovationDTModel(symoptim.Model):
         self.ny = ny
         """Number of outputs."""
         
-        self.iS_diag = [i==j for i,j in np.ndindex(nx, nx) if i<=j]
-        
+        # Define decision variables
         v = self.variables
-
-        # Define deicion variables
         v['x'] = [f'x{i}' for i in range(nx)]
         v['xn'] = [f'xn{i}' for i in range(nx)]
         v['xp'] = [f'xp{i}' for i in range(nx)]
@@ -33,7 +31,7 @@ class InnovationDTModel(symoptim.Model):
         v['C'] = [[f'C{i}_{j}' for j in range(nx)] for i in range(ny)]
         v['D'] = [[f'D{i}_{j}' for j in range(nu)] for i in range(ny)]        
         v['K'] = [[f'K{i}_{j}' for j in range(ny)] for i in range(nx)]
-        v['iS'] = [f'iS{i}_{j}' for i,j in np.ndindex(nx, nx) if i<=j]
+        v['iS'] = [f'iS{i}_{j}' for i,j in tril_ind(ny)]
         self.decision.update({k for k in v if k != 'self'})
 
         # Define auxiliary variables
@@ -48,28 +46,20 @@ class InnovationDTModel(symoptim.Model):
 
         # Mark extra functions to generate code
         self.generate_functions.update({'output', 'propagate'})
-    
-    def iS_mat(self, iS):
-        """Build the iS matrix from its nonzero elements."""
-        iS_mat = np.zeros((self.nx, self.nx), dtype=object)
-        tril_ind = ((i,j) for (i,j) in np.ndindex(self.nx, self.nx) if i<=j)
-        for ind, val in zip(tril_ind, iS):
-            iS_mat[ind] = val
-        return iS_mat
-    
+        
     def defects(self, xn, xp, up, yp, A, B, C, D, K, ybias):
         """Model dynamics defects."""
         return xn - self.propagate(xp, up, yp, A, B, C, D, K, ybias)
     
     def L(self, y, x, u, C, D, iS, ybias):
         """Measurement log-likelihood."""
-        iS_mat = self.iS_mat(iS)
+        iS = tril_mat(self.ny, iS)
         
         ymodel = self.output(x, u, C, D, ybias)
         e = y - ymodel
         
-        log_det_iS = sum(sympy.log(d) for d in iS_mat.diagonal())
-        L = -0.5 * e.T @ iS_mat.T @ iS_mat @ e + log_det_iS
+        log_det_iS = sum(sympy.log(d) for d in iS.diagonal())
+        L = -0.5 * e.T @ iS.T @ iS @ e + log_det_iS
         return L
     
     def output(self, x, u, C, D, ybias):
@@ -82,7 +72,76 @@ class InnovationDTModel(symoptim.Model):
     @property
     def generate_assignments(self):
         gen = {'nx': self.nx, 'nu': self.nu, 'ny': self.ny, 
-               'niS': len(self.variables['iS']), 'iS_diag': self.iS_diag,
+               'niS': len(self.variables['iS']),
+               'iS_diag': tril_diag(self.ny).tolist(),
                **getattr(super(), 'generate_assignments', {})}
         return gen
 
+
+class NaturalDTModel(InnovationDTModel):
+    def __init__(self, nx, nu, ny):
+        super().__init__(nx, nu, ny)
+        
+        # Define additional decision variables
+        v = self.variables
+        v['Pp'] = [[f'Pp{i}_{j}' for j in range(nx)] for i in range(nx)]
+        v['Pc'] = [[f'Pp{i}_{j}' for j in range(nx)] for i in range(nx)]
+        v['Q'] = [[f'Q{i}_{j}' for j in range(nx)] for i in range(nx)]
+        v['R'] = [[f'R{i}_{j}' for j in range(ny)] for i in range(ny)]
+        v['S'] = [f'S{i}_{j}' for i,j in tril_ind(ny)]
+        self.decision.update({'Pp', 'Pc', 'Q', 'R', 'S'})
+
+        # Register additional constraints
+        self.add_constraint('Pp_symmetry')
+        self.add_constraint('S_inverse')
+        self.add_constraint('output_cov')
+        self.add_constraint('x_pred_cov')
+        self.add_constraint('x_corr_cov')
+        self.add_constraint('kalman_gain')
+    
+    def Pp_symmetry(self, Pp):
+        return symmetry_constraint(Pp)
+    
+    def Pc_symmetry(self, Pc):
+        return symmetry_constraint(Pc)
+    
+    def S_inverse(self, iS, S):
+        iS = tril_mat(self.ny, iS)
+        S = tril_mat(self.ny, S)
+        I = np.eye(self.ny)
+        resid = S @ iS - I
+        return [resid[i] for i in tril_ind(self.ny)]
+    
+    def output_cov(self, C, Pp, R, S):
+        S = tril_mat(self.ny, S)
+        return C @ Pp @ C.T + R - S @ S.T
+    
+    def x_pred_cov(self, A, Pp, Pc, Q):
+        return A @ Pc @ A.T + Q - Pc
+    
+    def x_corr_cov(self, C, K, Pp, Pc):
+        return Pp - K @ C @ Pp - Pc
+    
+    def kalman_gain(self, C, K,  Pp, S):
+        S = tril_mat(self.ny, S)        
+        return Pp @ C - K @ S @ S.T
+
+
+def tril_ind(n):
+    yield from ((i,j) for (i,j) in np.ndindex(n, n) if i<=j)
+
+
+def tril_diag(n):
+    return np.array([i==j for (i,j) in tril_ind(n)])
+
+
+def tril_mat(n, elem):
+    mat = np.zeros((n, n), dtype=elem.dtype)
+    for ind, val in zip(tril_ind(n), elem):
+        mat[ind] = val
+    return mat
+
+
+def symmetry_constraint(M):
+    n = len(M)
+    return [M[i,j] - M[j,i] for (i,j) in np.ndindex(n, n) if i < j]
