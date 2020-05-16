47126ed096b572d20634691d478470d988a8dd6f
Dimas Abreu Archanjo Dutra <dimasad@ufmg.br>
2020-05-15 23:07:32 -0300

    Adding black-box system identification.

---
+++ blackbox_innov_bal.py
@@ -0,0 +1,121 @@
+"""Tests with the ATTAS aircraft short-period mode estimation."""
+
+
+import importlib
+import os
+
+import numpy as np
+import scipy.io
+import sympy
+import sym2num.model
+
+import fem
+import symfem
+
+
+# Reload modules for testing
+for m in (fem, symfem):
+    importlib.reload(m)
+
+
+def save_generated_model(symmodel):
+    clsname = type(symmodel).__name__
+    nx = symmodel.nx
+    nu = symmodel.nu
+    ny = symmodel.ny
+    with open(f'{clsname}_nx{nx}_nu{nu}_ny{ny}.py', mode='w') as f:
+        code = symmodel.print_code()
+        print(code, file=f)
+
+
+def get_model(nx, nu, ny):
+    clsname = 'InnovationBalDTModel'
+    modname = f'{clsname}_nx{nx}_nu{nu}_ny{ny}'
+    mod = importlib.import_module(modname)
+    
+    genclsname = f'Generated{clsname}'
+    cls = getattr(mod, genclsname)
+    return cls()
+
+
+def load_data():
+    # Retrieve data
+    u = np.loadtxt('/tmp/u.txt')
+    y = np.loadtxt('/tmp/y.txt')
+    return u, y
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    
+    nx = 3
+    nu = 2
+    ny = 3
+    
+    # Load experiment data
+    u, y = load_data()
+    symmodel = symfem.InnovationBalDTModel(nx=nx, nu=nu, ny=ny)
+    model = symmodel.compile_class()()
+    problem = fem.InnovationBalDTProblem(model, y, u)
+    N = len(y)
+    
+    # Define initial guess for decision variables
+    dec0 = np.zeros(problem.ndec)
+    var0 = problem.variables(dec0)
+    var0['A'][:] = np.loadtxt('/tmp/a.txt')
+    var0['B'][:] = np.loadtxt('/tmp/b.txt')
+    var0['C'][:] = np.loadtxt('/tmp/c.txt')
+    var0['D'][:] = np.loadtxt('/tmp/d.txt')
+    var0['Kp'][:] = np.loadtxt('/tmp/k.txt')
+    var0['x'][:] = np.loadtxt('/tmp/xpred.txt')
+    var0['W_diag'][:] = np.loadtxt('/tmp/gram.txt')
+    var0['isRp_tril'][:] = np.loadtxt('/tmp/isRp.txt')[np.tril_indices(ny)]
+    #var0['A'][:] = np.eye(nx)
+    #var0['C'][:] = np.eye(ny, nx)
+    #var0['Kp'][:] = np.eye(nx, ny)
+    #var0['isRp_tril'][symfem.tril_diag(ny)] = 100
+    
+    # Define bounds for decision variables
+    dec_bounds = np.repeat([[-np.inf], [np.inf]], problem.ndec, axis=-1)
+    dec_L, dec_U = dec_bounds
+    var_L = problem.variables(dec_L)
+    var_U = problem.variables(dec_U)
+    var_L['isRp_tril'][symfem.tril_diag(ny)] = 0
+    var_L['W_diag'][:] = 0
+    
+    # Define bounds for constraints
+    constr_bounds = np.zeros((2, problem.ncons))
+    constr_L, constr_U = constr_bounds
+    var_constr_L = problem.unpack_constraints(constr_L)
+    var_constr_U = problem.unpack_constraints(constr_U)
+    
+    # Define problem scaling
+    obj_scale = -1.0
+    constr_scale = np.ones(problem.ncons)
+    var_constr_scale = problem.unpack_constraints(constr_scale)
+    
+    dec_scale = np.ones(problem.ndec)
+    var_scale = problem.variables(dec_scale)
+    var_scale['isRp_tril'][:] = 1e-2
+    
+    with problem.ipopt(dec_bounds, constr_bounds) as nlp:
+        nlp.add_str_option('linear_solver', 'ma57')
+        nlp.add_num_option('tol', 1e-5)
+        nlp.add_num_option('ma57_pre_alloc', 5.0)
+        nlp.add_int_option('max_iter', 1000)
+        nlp.set_scaling(obj_scale, dec_scale, constr_scale)
+        decopt, info = nlp.solve(dec0)
+    
+    opt = problem.variables(decopt)    
+    xopt = opt['x']
+    A = opt['A']
+    B = opt['B']
+    C = opt['C']
+    D = opt['D']
+    Kp = opt['Kp']
+    W = np.diag(opt['W_diag'])
+    ybias = opt['ybias']
+    isRp = symfem.tril_mat(ny, opt['isRp_tril'])
+    yopt = model.output(xopt, u, C, D, ybias)
+    eopt = y - yopt
+
---
+++ mc_data_gen.m
@@ -0,0 +1,72 @@
+%%%% Data generation for Monte-Carlo empirical study %%%%
+%% Setup random number generator and define parameters
+rng(0)
+
+nx = 3;
+nu = 2;
+ny = 3;
+nw = nx;
+
+N = 500;
+
+std_u = 1;
+std_w = 0.05;
+std_v = 0.2;
+
+%% Create system
+rng_settings = rng;
+
+sys = drss(nx, ny, nu + nw);
+sys.d(:, nu+1:end) = 0;
+
+Q = eye(nw) * std_w^2;
+R = eye(ny) * std_v^2;
+
+[~, L, Pp] = kalman(sys, Q, R);
+
+u = std_u * randn(N, nu);
+w = std_w * randn(N, nw);
+v = std_v * randn(N, ny);
+
+y = lsim(sys, [u, w]) + v;
+
+%% Save
+save -ascii /tmp/u.txt u
+save -ascii /tmp/y.txt y
+
+%% 
+d = iddata(y, u, 1);
+syse = n4sid(d, nx);
+[syseb, gram, T] = balreal(syse);
+A = syseb.a;
+B = syseb.b;
+C = syseb.c;
+D = syseb.d;
+K = T * syse.k;
+
+xpred = zeros(N, nx);
+for i=1:N-1
+    ui = u(i, :)';
+    xi = xpred(i, :)';
+    yi = y(i, :)';
+    ei = yi - C*xi - D*ui;
+    xnext = A*xi + B*ui + K*ei;
+    xpred(i+1, :) = xnext';
+end
+ypred = xpred * C' + u*D';
+
+Rp = cov(y-ypred);
+sRp = chol(Rp)';
+isRp = inv(sRp);
+
+%% 
+save -ascii /tmp/a.txt A
+save -ascii /tmp/b.txt B
+save -ascii /tmp/c.txt C
+save -ascii /tmp/d.txt D
+save -ascii /tmp/k.txt K
+save -ascii /tmp/xpred.txt xpred
+save -ascii /tmp/gram.txt gram
+save -ascii /tmp/isRp.txt isRp
+
+%%
